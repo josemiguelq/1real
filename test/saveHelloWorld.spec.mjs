3704e8fd7dc972121874f103b1cfb93161be5e09
Vania Jucá <vaniajuca@Vitor-Juca.local>
2018-07-25 12:26:05 -0400

    Added save test

--- test/saveHelloWorld.spec.mjs
+++ test/saveHelloWorld.spec.mjs
@@ -1,10 +1,10 @@
 import request from 'supertest'
-import app from '../src/index.mjs'
+import app from '../src/app.mjs'
 import Server from '../src/common/Server.mjs'
 import environment from '../src/common/environment.mjs'
 import logger from '../src/common/logger.mjs'
 
-describe('Register', () => {
+describe('Save HelloWorld', () => {
     before((done) => {
         const server = new Server(app, environment)
         server.start()
@@ -15,24 +15,23 @@ describe('Register', () => {
             .catch(err => logger.error('Error on starting server %s', err))
     })
 
-    it('deve registrar novo usuario quando todos os atributos forem informados', (done) => {
-        const userData = {email: 'test5@test.com.br', password: 'senha123'}
+    it('should save when data is valid', (done) => {
+        const payload = {hello_world: 'Olá Mundo'}
         request(app)
-            .post('/api/auth/register')
-            .send(userData)
+            .post('')
+            .send(payload)
             .end((err, res) => {
                 expect(res.statusCode).to.equal(200)
                 expect(res.body).to.have.property('_id')
-                expect(res.body.email).to.equal(userData.email)
                 done()
             })
     })
 
-    it('deve devolver status 500 quando o email não for informado', (done) => {
-        const userData = {password: 'senha123'}
+    it('should send status 500 when payload is invalid', (done) => {
+        const payload = 'undefined'
         request(app)
-            .post('/api/auth/register')
-            .send(userData)
+            .post('/')
+            .send(payload)
             .end((err, res) => {
                 expect(res.statusCode).to.equal(500)
                 done()
