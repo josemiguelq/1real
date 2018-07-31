233f2d9d5009e529135baeb61c23062430e1543e
Vitor Jucá <vaniajuca@Vitor-Juca.local>
2018-07-31 17:49:30 -0400

    change routes to post and get

--- src/app.mjs
+++ src/app.mjs
@@ -2,15 +2,19 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import helmet from 'helmet'
-import errorHandler from './common/errorHandler.mjs'
-import helloworld from './router/HelloWorld.mjs'
+import GET from './router/GET.mjs'
+import POST from './router/POST.mjs'
+import RouteLogger from './common/RouteLogger.mjs'
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(helmet())
 app.use(cors())
-app.use(helloworld)
-app.use(errorHandler)
+app.use(RouteLogger)
+
+/** Insert your routes here */
+app.use(GET)
+app.use(POST)
 
 export default app
