5d26fb905bc616bb1b317493dda9d02e332588d7
Vitor Juca <vitor.juca222@gmail.com>
2018-11-19 01:01:54 -0200

    .

--- src/app.mjs
+++ src/app.mjs
@@ -17,7 +17,7 @@ app.set('view engine', 'twig');
 app.use(express.static(path.join(path.resolve('./src'), 'public')));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-app.use(helmet({frameguard:false}))
+app.use(helmet({frameguard:false, noCache:true}))
 app.use(cors())
 app.use(RouteLogger)
 
